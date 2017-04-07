eefe5ef85f34b66ae59d7184ca99f3168361e1c1
DESKTOP-U7T3T39\thoma <thomas.m.mallia@gmail.com>
2017-04-07 11:25:40 +0200

    Added functionality to Map

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,17 +1,38 @@
+package main.java;
+
 /**
  * Created by rodemic on 07/04/2017.
  */
 
 //TODO
 public class Map {
-    int size;
-    boolean setMapSize(int x, int y){
-        return true;
+    private int size;
+    private char[][] map = new char[size][size];
+
+    public Map(int s){
+        size = s;
     }
-    void generate(){
+
+    public boolean setMapSize(int s, int player){
+        if(player >= 2 && player <= 4 && s >= 5){
+            size = s;
+            return true;
+        }
+        else if(player >= 5 && player <= 8 && s >= 8){
+            size = s;
+            return true;
+        }
+        else return false;
+    }
+
+    public void generate(){
+        int xrand = (int)(Math.random()*size + 1);
+        int yrand = (int)(Math.random()*size + 1);
+        map[xrand][yrand] = 'T';
 
     }
-    char getTileType(int x, int y){
-        return 'G';
-    }
+
+    public int getMapSize(){ return size; }
+
+    public char getTileType(int x, int y){ return map[x][y];}
 }

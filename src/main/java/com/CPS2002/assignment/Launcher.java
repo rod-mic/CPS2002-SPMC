284482c8b080b4e69514d1df0589f543e0f35d36
Roderick Micallef <roderick.micallef.15@um.edu.mt>
2017-04-07 16:06:45 +0200

    Fixed input being skipped

--- src/main/java/com/CPS2002/assignment/Launcher.java
+++ src/main/java/com/CPS2002/assignment/Launcher.java
@@ -16,31 +16,31 @@ public class Launcher {
         int numPlayer = 0;
         int mapSize;
         Map map = new Map();
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");;
 
         System.out.println("Welcome to the game!");
         System.out.println("How many users will be playing? (2-8 Players)");
+
         while(!check){
             try {
-                numPlayer = sc.nextInt();
+                numPlayer = Integer.parseInt(sc.nextLine());
                 if(numPlayer < 2 || numPlayer > 8)throw new SizeException("2 and 8");
                 check = true;
             }catch(InputMismatchException ex){
                 System.out.println("Incorrect input. Numbers only. Try again: ");
-                sc.nextLine();
                 check = false;
             }catch(SizeException ex){
-                System.out.println("There can only be between "+ex+" players. Try again: ");
-                sc.nextLine();
+                System.out.println("There can only be between "+ex.getMessage()+" players. Try again: ");
                 check = false;
             }
-        }
+        }check = false;
 
         if(numPlayer < 5) System.out.println("How big  will the map be? (Between 5x5 and 50x50)");
         else System.out.println("How big  will the map be? (Between 8x8 and 50x50)");
+
         while(!check){
             try {
-                mapSize = sc.nextInt();
+                mapSize = Integer.parseInt(sc.nextLine());
                 if(!map.setMapSize(mapSize,numPlayer)) {
                     if ((mapSize < 5 || mapSize > 50) && numPlayer < 5) throw new SizeException("5x5 and 50x50");
                     else if ((mapSize < 8 || mapSize > 50) && numPlayer > 4) throw new SizeException("8x8 and 50x50");
@@ -48,13 +48,15 @@ public class Launcher {
                 check = true;
             }catch(InputMismatchException ex){
                 System.out.println("Incorrect input. Numbers only. Try again: ");
-                sc.nextLine();
                 check = false;
             }catch(SizeException ex){
-                System.out.println("The size must be between "+ex+" tiles. Try again: ");
-                sc.nextLine();
+                System.out.println("The size must be between "+ex.getMessage()+" tiles. Try again: ");
                 check = false;
             }
+        }
+
+        for(int i = 0; i < numPlayer; i++){
+
         }
     }
 

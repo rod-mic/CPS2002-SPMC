ad5c050a4de559b06f7f435a14686b680e93a148
DESKTOP-U7T3T39\thoma <thomas.m.mallia@gmail.com>
2017-04-07 14:55:25 +0200

    fixed issue in generate

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -33,13 +33,14 @@ public class Map {
     }
 
     public void generate(){
-        int xrand = (int)(Math.random()*size);
-        int yrand = (int)(Math.random()*size);
+        int xrand = (int)(Math.random()*(size-1));
+        int yrand = (int)(Math.random()*(size-1));
         map[xrand][yrand] = 'T';
         int randomValue;
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                randomValue = (int)(Math.random()*6 +1);
+                randomValue = (int)(Math.random()*6);
+                System.out.println(randomValue);
                 if(map[i][j] != 'T'){
                     if(randomValue == 5) map[i][j] = 'W';
                     else map[i][j] = 'G';

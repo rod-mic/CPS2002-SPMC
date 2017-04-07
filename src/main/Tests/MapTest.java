64c66d1896d8bc89c19dea0d75053a9fd12864ae
DESKTOP-U7T3T39\thoma <thomas.m.mallia@gmail.com>
2017-04-07 14:17:30 +0200

    Added tests for Map and removed AlgoLayout file

---
+++ src/main/Tests/MapTest.java
@@ -0,0 +1,41 @@
+package main.Tests;
+
+import main.java.Map;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by thoma on 07/04/2017.
+ */
+public class MapTest {
+    Map m;
+
+    @org.junit.Test
+    public void setMap_2player_goodSize(){
+        assertEquals(true, m.setMapSize(6,3));
+    }
+
+    @org.junit.Test
+    public void setMap_5player_goodSize(){
+        assertEquals(true, m.setMapSize(9,5));
+    }
+
+    @org.junit.Test
+    public void setMap_2player_badSize(){
+        assertEquals(false, m.setMapSize(4,3));
+    }
+
+    @org.junit.Test
+    public void setMap_5player_badSize(){
+        assertEquals(false, m.setMapSize(7,6));
+    }
+
+    @org.junit.Test
+    public void setMap_below_Min_Player(){
+        assertEquals(false, m.setMapSize(5,1));
+    }
+
+    @org.junit.Test
+    public void setMap_above_Max_Player(){
+        assertEquals(false, m.setMapSize(8,9));
+    }
+}

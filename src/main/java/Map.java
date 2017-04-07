6135e0bc5666035ee9cc14bde5cf9ca205dd14ce
roderick micallef <roderick micallef>
2017-04-07 11:10:33 +0200

    Added shell files

---
+++ src/main/java/Map.java
@@ -0,0 +1,17 @@
+/**
+ * Created by rodemic on 07/04/2017.
+ */
+
+//TODO
+public class Map {
+    int size;
+    boolean setMapSize(int x, int y){
+        return true;
+    }
+    void generate(){
+
+    }
+    char getTileType(int x, int y){
+        return 'G';
+    }
+}

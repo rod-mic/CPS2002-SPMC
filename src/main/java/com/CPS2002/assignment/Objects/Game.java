f4ec1688de9f98d20c89c4b9906bad94d9f13181
Roderick Micallef <roderick.micallef.15@um.edu.mt>
2017-04-07 21:18:05 +0200

    Finished generateHTMLFiles

--- src/main/java/com/CPS2002/assignment/Objects/Game.java
+++ src/main/java/com/CPS2002/assignment/Objects/Game.java
@@ -1,5 +1,13 @@
 package com.CPS2002.assignment.Objects;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static java.lang.System.exit;
+
 /**
  * Created by rodemic on 07/04/2017.
  */
@@ -16,6 +24,7 @@ public class Game {
     }
 
     boolean startGame(){
+        generateHTMLFiles();
         return true;
     }
 
@@ -26,15 +35,41 @@ public class Game {
                 table += "<tr>\n";
                 for(int k = 0;k < map.getMapSize();k++){
                     Position p = new Position(j,k);
-                    if(p == players[i].getStartPosition()){
-                        table += "<th class=\"grass\">&#9905;</th>\n";
+                    if(players[i].getStartPosition().equals(p)) {
+                        switch(map.getTileType(p)){
+                            case 'G': table += "<th class=\"grass\">&#9905;</th>\n";break;
+                            case 'W': table += "<th class=\"water\">&#9905;</th>\n";break;
+                            case 'T': table += "<th class=\"treasure\">&#9905;</th>\n";break;
+                        }
+                    }else if(players[i].getPreviousPositions().contains(p)){
+                        switch(map.getTileType(p)){
+                            case 'G': table += "<th class=\"grass\"></th>\n";break;
+                            case 'W': table += "<th class=\"water\"></th>\n";break;
+                            case 'T': table += "<th class=\"treasure\"></th>\n";break;
+                        }
                     }else{
                         table += "<th class=\"unexplored\"></th>\n";
                     }
                 }
                 table += "</tr>\n";
             }
-            System.out.println(table);
+            Path templatePath = Paths.get("../html/template.html");
+            String content = "";
+            try {
+                content = new String(Files.readAllBytes(templatePath));
+                content = content.replace("$table",table);
+            } catch (java.io.IOException e) {
+                System.out.println("File htmlTemplate not found. Exiting Program");
+                exit(1);
+            }
+            Path playerPath = Paths.get("../html/map_player_"+ Integer.toString(i) + ".html");
+            try {
+                Files.write(playerPath,content.getBytes(), StandardOpenOption.CREATE);
+            } catch (IOException e) {
+                System.out.println("File map_player_"+Integer.toString(i)+ ".html not found. Exiting Program");
+                exit(1);
+            }
+
         }
     }
 

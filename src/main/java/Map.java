a7318122995e12783e6bafa602f9f40a9d93a5d7
DESKTOP-U7T3T39\thoma <thomas.m.mallia@gmail.com>
2017-04-07 14:50:36 +0200

    added function in Map to initialise map array

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -11,18 +11,21 @@ public class Map {
     private int size;
     private char[][] map;
 
-    public Map(){}
+    public Map(){
+    }
 
     public boolean setMapSize(int s, int player){
         if(player >= 2 && player <= 4 && s >= 5){
             size = s;
             map = new char[size][size];
+            initMap(size);
             generate();
             return true;
         }
         else if(player >= 5 && player <= 8 && s >= 8){
             size = s;
             map = new char[size][size];
+            initMap(size);
             generate();
             return true;
         }
@@ -53,5 +56,13 @@ public class Map {
         Graph g = new Graph(map,position);
         BreathFirst b = new BreathFirst(g);
         return b.checkPath();
+    }
+
+    public void initMap(int size){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                map[i][j] = ' ';
+            }
+        }
     }
 }

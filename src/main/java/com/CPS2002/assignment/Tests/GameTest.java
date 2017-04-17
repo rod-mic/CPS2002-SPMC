f7f6c3698bb351629147e35f62c80316e31bdf6e
Roderick Micallef <roderick.micallef.15@um.edu.mt>
2017-04-17 13:04:36 +0200

    Fixed incorrect upload of GameTest

--- src/main/java/com/CPS2002/assignment/Tests/GameTest.java
+++ src/main/java/com/CPS2002/assignment/Tests/GameTest.java
@@ -1,46 +1,84 @@
 package com.CPS2002.assignment.Tests;
 
-
+import com.CPS2002.assignment.Objects.Game;
 import com.CPS2002.assignment.Objects.Map;
+import com.CPS2002.assignment.Objects.Player;
+import com.CPS2002.assignment.Objects.Position;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertEquals;
 
+/**
+ * Created by rodemic on 17/04/2017.
+ */
 public class GameTest {
-    final private Map m = new Map();
-
-    @org.junit.Test
-    public void setMap_2player_goodSize(){
-        assertEquals(true, m.setMapSize(6,3));
+    Game g;
+    Player[] p = new Player[2];
+    Map m;
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    @Before
+    public void setup() throws Exception{
+        m = new Map();
+        m.setMapSize(5,2);
+        p[0] = new Player(m);
+        p[1] = new Player(m);
+        g = new Game(p,m);
     }
 
-    @org.junit.Test
-    public void setMap_5player_goodSize(){
-        assertEquals(true, m.setMapSize(9,5));
+
+    @Test
+    public void movePlayers() throws Exception {
+        char[] direction = {'U','D'};
+        g.movePlayers(direction);
     }
 
-    @org.junit.Test
-    public void setMap_2player_badSize(){
-        assertEquals(false, m.setMapSize(4,3));
+    @Test
+    public void checkWinner() throws Exception {
+        Position pos = p[0].getPosition();
+        if(m.getTileType(pos) == 'T')assertEquals(true,g.checkWinner(p[0]));
+        else assertEquals(false,g.checkWinner(p[0]));
     }
 
-    @org.junit.Test
-    public void setMap_5player_badSize(){
-        assertEquals(false, m.setMapSize(7,6));
+    @Test
+    public void checkWater() throws Exception {
+        Position pos = p[0].getPosition();
+        if(m.getTileType(pos) == 'W')assertEquals(true,g.checkWater(p[0]));
+        else assertEquals(false,g.checkWater(p[0]));
     }
 
-    @org.junit.Test
-    public void setMap_below_Min_Player(){
-        assertEquals(false, m.setMapSize(5,1));
+    @Test
+    public void printOneWinner() throws Exception {
+        ArrayList<Integer> winners = new ArrayList<>();
+        winners.add(1);
+        String output = "Player 1 found the Treasure!\n";
+        g.printWinners(winners);
+        assertEquals(output,systemOutRule.getLog());
     }
 
-    @org.junit.Test
-    public void setMap_above_Max_Player(){
-        assertEquals(false, m.setMapSize(8,9));
+    @Test
+    public void printMultipleWinners() throws Exception {
+        ArrayList<Integer> winners = new ArrayList<>();
+        winners.add(1);
+        winners.add(2);
+        winners.add(3);
+        String output = "Players 1 2 3 found the Treasure!\n";
+        g.printWinners(winners);
+        assertEquals(output,systemOutRule.getLog());
     }
 
-    @org.junit.Test
-    public void get_map_size(){
-        m.setMapSize(5,4);
-        assertEquals(5,m.getMapSize());
+    @Test
+    public void cleanup() throws Exception {
+
+    }
+
+    @Test
+    public void generateHTMLFiles1() throws Exception {
+
     }
 }

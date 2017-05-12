d75f482946f2696eccd520b2251f6cfcc6bdb57d
DESKTOP-U7T3T39\thoma <thomas.m.mallia@gmail.com>
2017-05-12 14:39:33 +0200

    Added comments to Launcher class

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -10,14 +10,17 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * Created by rodemic on 07/04/2017.
  */
+
 public class Launcher {
 
+    //Main method for running game
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        Scanner sc = new Scanner(System.in).useDelimiter("\n"); //creates a Scanner Object for use of user input
         Game game = setup(sc);
-        game.startGame();
+        game.startGame();   //Begins the game
     }
 
+    //Function for getting input from user taking a minimum and maximum input
     public static int getIntegerInput(int min, int max, Scanner sc) {
         boolean check;
         int input = min - 1;
@@ -26,10 +29,10 @@ public class Launcher {
                 input = Integer.parseInt(sc.nextLine());
                 if (input < min || input > max) throw new SizeException(min + " and " + max);
                 check = true;
-            } catch (NumberFormatException ex) {
+            } catch (NumberFormatException ex) { //Handles input of non-digits
                 System.out.print("Incorrect input. Numbers only. Try again: ");
                 check = false;
-            } catch (SizeException ex) {
+            } catch (SizeException ex) {         //Handles when input is below or above the min or the maximum respectively
                 System.out.print("The range is between " + ex.getMessage() + ". Try again: ");
                 check = false;
             }
@@ -37,39 +40,42 @@ public class Launcher {
         return input;
     }
 
+    //Function that requests user to input the setting for the game
     public static Game setup(Scanner sc) {
+
+        //Settings variable initialisation
         int numPlayer;
         int mapSize;
         int mapType;
         int gameType;
 
         Player[] players;
-        Map map = new Map();
+        Map map = new Map(); //creates Map Object
         Game game;
 
         System.out.println("Welcome to the game!");
-        System.out.println("Choose game type: (1) Individual - (2) Collaborative");
+        System.out.println("Choose game type: (1) Individual - (2) Collaborative"); //Choice for whether players to play in teams or solo
 
         gameType = getIntegerInput(1, 2, sc);
 
-        System.out.println("Choose map type: (1) Safe - (2) Hazardous");
+        System.out.println("Choose map type: (1) Safe - (2) Hazardous"); //Choice between two map types, safe and hazardous
 
         mapType = getIntegerInput(1, 2, sc);
 
-        System.out.println("How many users will be playing? (2-8 Players)");
+        System.out.println("How many users will be playing? (2-8 Players)"); //Choice for amount of players in game
 
         numPlayer = getIntegerInput(2, 8, sc);
         players = new Player[numPlayer];
         if (numPlayer < 5) {
-            System.out.println("How big  will the map be? (Between 5x5 and 50x50)");
+            System.out.println("How big  will the map be? (Between 5x5 and 50x50)"); //Choice for map size
             mapSize = getIntegerInput(5, 50, sc);
         } else {
             System.out.println("How big  will the map be? (Between 8x8 and 50x50)");
             mapSize = getIntegerInput(8, 50, sc);
         }
-        map.setMapSize(mapSize, numPlayer, mapType);
+        map.setMapSize(mapSize, numPlayer, mapType); //generates the map according to the specifications given
 
-        if (gameType == 2) {
+        if (gameType == 2) { //Section of code for setting up teams
             int max = numPlayer-1;
             System.out.println("How many teams will be playing? (2-" + max + " Teams)");
             int numTeam = getIntegerInput(2, max, sc);
@@ -80,8 +86,8 @@ public class Launcher {
             }
 
 
-            int playerCounter = 0;
-            int teamCounter = 0;
+            int playerCounter = 0; //counter for keeping track of players in a team
+            int teamCounter = 0;   //counter for keeping track of team number index
             while(playerCounter < numPlayer){
 
                 if(teamCounter >= numTeam)
@@ -92,22 +98,22 @@ public class Launcher {
                 Team currentTeam = teams[teamCounter];
 
                 do {
-                    randPlayer = ThreadLocalRandom.current().nextInt(0, numPlayer);
+                    randPlayer = ThreadLocalRandom.current().nextInt(0, numPlayer); //choices a random player that has not yet been put in team
                     chosenPlayer = players[randPlayer];
                 } while (chosenPlayer != null);
 
                 System.out.println("Player "+randPlayer+" added to team "+teamCounter);
-                players[randPlayer] = new Player(map,currentTeam);
+                players[randPlayer] = new Player(map,currentTeam); //creates a Player Object
                 teamCounter++;
                 playerCounter++;
             }
-        } else {
+        } else { //Setup for solo play
             for (int i = 0; i < numPlayer; i++) {
                 players[i] = new Player(map);
             }
         }
-        game = new Game(players, map);
-        game.generateHTMLFiles();
+        game = new Game(players, map); //creates a Game Object taking the set of players and created map
+        game.generateHTMLFiles();   //generates html files for each player
         return game;
     }
 }
